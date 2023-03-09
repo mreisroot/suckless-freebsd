96fc00aa2f2c6be1d971353c086e4f7224aafdd3
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-09 14:17:46 -0300

    Update provision script

--- provision.sh
+++ provision.sh
@@ -2,11 +2,10 @@
 
 home=/home/vagrant
 
-sudo su <<- EOF
-  pkg update
-  pkg install -y git
-  printf "vagrant\nvagrant" | passwd vagrant
-EOF
+
+pkg update
+pkg install -y git
+printf "vagrant\nvagrant" | passwd vagrant
 
 # Clone suckless-freebsd script repo
 git clone -b dev https://gitlab.com/mreisroot/suckless-freebsd.git "${home}"/suckless-freebsd/
