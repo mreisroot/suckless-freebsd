512eb71eb40a5d4432597eeb5baac9db5da74bda
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-09 14:08:28 -0300

    Update testing script

--- suckless-freebsd.sh
+++ suckless-freebsd.sh
@@ -4,38 +4,31 @@
 user=vagrant
 home=/home/$user
 
-sudo su <<- EOF
+# Variables
+dependencies="xorg-minimal xorg-fonts xf86-video-intel libx11 mesa-devel libx11 libxft libxinerama xinit xrdb xrandr unclutter xclip xbacklight redshift rxvt-unicode vim tmux lynx pcmanfm"
+tools="dwm dmenu st slstatus"
 
-  # Variables
-  user="$user"
-  home="$home"
-  dependencies="xorg-minimal xorg-fonts xf86-video-intel libx11 mesa-devel libx11 libxft libxinerama xinit xrdb xrandr unclutter xclip xbacklight redshift rxvt-unicode vim tmux lynx pcmanfm"
-  tools="dwm dmenu st slstatus"
+# Upgrade system and install dependencies
+freebsd-update fetch install
+pkg update && pkg upgrade -y
+pkg install -y $dependencies
 
-  # Upgrade system and install dependencies
-  freebsd-update fetch install
-  pkg update && pkg upgrade -y
-  pkg install -y \$dependencies
+# Clone repos
+for i in $tools;
+do
+  git clone -b freebsd https://gitlab.com/mreisroot/${i}.git ${home}/.config/src/${i};
+done
 
-  # Clone repos
-  for i in \$tools;
-  do
-    git clone -b freebsd https://gitlab.com/mreisroot/\${i}.git \${home}/.config/src/\${i};
-  done
+# Compile tools
+for i in $tools;
+do
+  cd ${home}/.config/src/${i};
+  make clean install;
+done
 
-  # Compile tools
-  for i in \$tools;
-  do
-    cd \${home}/.config/src/\${i};
+# Copy .xinitrc to home directory
+cp ${home}/suckless-freebsd/.xinitrc ${home}/.xinitrc
 
-    make clean install;
-  done
-
-  # Copy .xinitrc to home directory
-  cp \${home}/suckless-freebsd/.xinitrc \${home}/.xinitrc
-
-  # Adjust permissions
-  chown -R \${user}:\${user} \${home}/.config
-  chown \${user}:\${user} \${home}/.xinitrc
-
-EOF
+# Adjust permissions
+chown -R ${user}:${user} ${home}/.config
+chown ${user}:${user} ${home}/.xinitrc
