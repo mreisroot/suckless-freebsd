f0664ae0905a15d0d71be0de72ecdc473bbc6f61
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-13 12:24:26 -0300

    Update script

--- suckless-freebsd.sh
+++ suckless-freebsd.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 # user and home directory
-set user = $USER
-set home = $HOME
+user $USER
+home $HOME
 
 su - <<- EOF
+#!/bin/sh
 
   # Variables
-  set user = "$user"
-  set home = "$home"
-  set dependencies = "xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
-  set tools = "dwm dmenu st slstatus"
+  user="$user"
+  home="$home"
+  dependencies="xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
+  tools="dwm dmenu st slstatus"
 
   # Upgrade system and install dependencies
   freebsd-update fetch install
@@ -19,27 +20,27 @@ su - <<- EOF
 
   # Install DevOps tools
   printf "\nDo you want to install DevOps tools? (y/n) "
-  $< ans
+  read -r ans
   if [ \$ans = 'y' || \$ans = 'Y' ]; then
-    set devops = "py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
+    devops="py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
     pkg install -y \$devops
   fi
 
   # Install LaTeX
   printf "\nDo you want to install LaTeX? (y/n) "
-  $< ans2
+  read -r ans2
   if [ \$ans2 = 'y' || \$ans2 = 'Y' ]; then
     pkg install -y texlive-full latex-biber
   fi
 
   # Clone repos
-  foreach i (\$tools);
+  for i in \$tools;
   do
     git clone -b freebsd https://gitlab.com/mreisroot/\${i}.git \${home}/.config/src/\${i};
   done
 
   # Compile tools
-  foreach i (\$tools);
+  for i in \$tools;
   do
     cd \${home}/.config/src/\${i};
 
@@ -52,5 +53,4 @@ su - <<- EOF
   # Adjust permissions
   chown -R \${user}:\${user} \${home}/.config
   chown \${user}:\${user} \${home}/.xinitrc
-
 EOF
