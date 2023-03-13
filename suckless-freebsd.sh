5a068b81d428948c40655fc7ab06a875be98e84d
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-13 11:43:40 -0300

    Update script

--- suckless-freebsd.sh
+++ suckless-freebsd.sh
@@ -4,18 +4,33 @@
 user=$USER
 home=$HOME
 
-sudo su <<- EOF
+su - <<- EOF
 
   # Variables
   user="$user"
   home="$home"
-  dependencies="xorg-minimal xorg-fonts xf86-video-intel libx11 mesa-devel libx11 libxft libxinerama xinit xrdb xrandr unclutter xclip xbacklight redshift rxvt-unicode vim tmux lynx pcmanfm"
+  dependencies="xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
   tools="dwm dmenu st slstatus"
 
   # Upgrade system and install dependencies
   freebsd-update fetch install
   pkg update && pkg upgrade -y
   pkg install -y \$dependencies
+
+  # Install DevOps tools
+  printf "\nDo you want to install DevOps tools? (y/n) "
+  read -r ans
+  if [ \$ans = 'y' || \$ans = 'Y' ]; then
+    devops="py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
+    pkg install -y \$devops
+  fi
+
+  # Install LaTeX
+  printf "\nDo you want to install LaTeX? (y/n) "
+  read -r ans2
+  if [ \$ans2 = 'y' || \$ans2 = 'Y' ]; then
+    pkg install -y texlive-full latex-biber
+  fi
 
   # Clone repos
   for i in \$tools;
