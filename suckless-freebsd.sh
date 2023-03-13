4f37dc5dcfc0c4160a2482929a9375ce250e060b
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-13 12:02:19 -0300

    Update script

--- suckless-freebsd.sh
+++ suckless-freebsd.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # user and home directory
-user=$USER
-home=$HOME
+set user = $USER
+set home = $HOME
 
 su - <<- EOF
 
   # Variables
-  user="$user"
-  home="$home"
-  dependencies="xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
-  tools="dwm dmenu st slstatus"
+  set user = "$user"
+  set home = "$home"
+  set dependencies = "xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
+  set tools = "dwm dmenu st slstatus"
 
   # Upgrade system and install dependencies
   freebsd-update fetch install
@@ -21,7 +21,7 @@ su - <<- EOF
   printf "\nDo you want to install DevOps tools? (y/n) "
   read -r ans
   if [ \$ans = 'y' || \$ans = 'Y' ]; then
-    devops="py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
+    set devops = "py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
     pkg install -y \$devops
   fi
 
@@ -33,13 +33,13 @@ su - <<- EOF
   fi
 
   # Clone repos
-  for i in \$tools;
+  foreach i (\$tools);
   do
     git clone -b freebsd https://gitlab.com/mreisroot/\${i}.git \${home}/.config/src/\${i};
   done
 
   # Compile tools
-  for i in \$tools;
+  foreach i (\$tools);
   do
     cd \${home}/.config/src/\${i};
 
