656491682e6e0e9f762722f7f75d580657faca2a
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-13 12:30:47 -0300

    Update script

--- suckless-freebsd.sh
+++ suckless-freebsd.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # user and home directory
-user=$USER
-home=$HOME
+set user = $USER
+set home = $HOME
 
 su - <<- EOF
-#!/bin/sh
 
   # Variables
-  user="$user"
-  home="$home"
-  dependencies="xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
-  tools="dwm dmenu st slstatus"
+  set user = "$user"
+  set home = "$home"
+  set dependencies = "xorg-minimal mesa-devel libXft libXinerama xinit xrdb xrandr unclutter xclip xbacklight doas password-store pass-otp pkgconf vim tmux lynx wget curl mixertui yt-dlp pipe-viewer cups sane-backends freetype2 redshift rxvt-unicode mpv zathura zathura-ps zathura-pdf-mupdf zathura-djvu pcmanfm-gtk3"
+  set tools = "dwm dmenu st slstatus"
 
   # Upgrade system and install dependencies
   freebsd-update fetch install
@@ -20,32 +19,29 @@ su - <<- EOF
 
   # Install DevOps tools
   printf "\nDo you want to install DevOps tools? (y/n) "
-  read -r ans
+  set ans = $<
   if [ \$ans = 'y' || \$ans = 'Y' ]; then
-    devops="py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
+    set devops = "py39-ansible py39-ansible-lint terraform gh glab packer vagrant virtualbox-ose kubectl go"
     pkg install -y \$devops
   fi
 
   # Install LaTeX
   printf "\nDo you want to install LaTeX? (y/n) "
-  read -r ans2
+  set ans2 = $<
   if [ \$ans2 = 'y' || \$ans2 = 'Y' ]; then
     pkg install -y texlive-full latex-biber
   fi
 
   # Clone repos
-  for i in \$tools;
-  do
+  foreach i (\$tools)
     git clone -b freebsd https://gitlab.com/mreisroot/\${i}.git \${home}/.config/src/\${i};
-  done
+  end
 
   # Compile tools
-  for i in \$tools;
-  do
+  foreach i (\$tools)
     cd \${home}/.config/src/\${i};
-
     make clean install;
-  done
+  end
 
   # Copy .xinitrc to home directory
   cp \${home}/suckless-freebsd/.xinitrc \${home}/.xinitrc
